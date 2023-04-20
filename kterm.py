fecc3fc73263bc275e6520e99d49926bcd35850f
Peter Stoeck <kpstoeck@web.de>
2023-04-20 23:00:49 +0200

    Wlan integriert.

--- kterm.py
+++ kterm.py
@@ -1,16 +1,25 @@
 # kterm 
 # ist ein Micropythonscript für ein einfaches Thermometer 
 # mit einem M5Stick C und einer K-Meter Unit.
+# Über ein Webinterface können Einstellungen vorgenommen werden
+# und die Temperatur abgefragt werden.
 
 name = 'kterm.py'
-version = '00.00.005'
+version = '00.01.000'
 date = '20.04.2023'
 author = 'Peter Stöck'
 
 # TODO:
-# 
+# Fehlerbehandlung wenn dev_config.json
+# nicht geladen werden kann.
 
 # Versionen:
+# 00.01.000:
+# dev_config.json integriert.
+# Wlan integriert.
+# Anzeige angepasst.
+# y_offset_temperatur eingeführt.
+#
 # 00.00.005:
 # Farbgebung auch für °C implementiert.
 # ° 2 Pixel höher gesetzt.
@@ -34,6 +43,9 @@ author = 'Peter Stöck'
 
 from m5imports import *
 import unit
+import network
+from wlansecrets import SSID, PW
+import json
 
 temp = '20.0'
 diplay_breite = 160
@@ -51,22 +63,67 @@ alarm_farbe = 0xff0000
 x_offset_2stellig = 35
 x_offset_3stellig = 10
 
+y_offset_temperatur = 37
+
 lcd.setRotation(3)
 
 setScreenColor(0x404040)
 KMeter_0 = unit.get(unit.KMETER, unit.PORTA)
 
+
+##########################################
+# Geräte Definitionen laden.
+##########################################
+  
+try:
+    f = open('dev_config.json','r')
+    dc = f.read()
+    f.close()
+    dev_config = json.loads(dc)
+except:
+    print('dev_config.json konnte nicht geladen werden.')
+#    write_log('dev_config.json konnte nicht geholt werden!')
+#    abbruch = True
+
+
+##########################################
+# Wlan einrichten und verbinden:
+##########################################
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+wlan.ifconfig((dev_config['fixIP'], '255.255.255.0', '192.168.5.1', '192.168.5.1'))
+
+wlan.connect(SSID, PW)
+time_out = 10
+while not wlan.isconnected():
+    time.sleep(1)
+    time_out -= 1
+    if time_out == 0:
+        print('Keine Wlan Verbindung!')
+        # write_log('Wlan nicht gefunden.')
+        # abbruch = True
+        break
+
+print('IP: '+ wlan.ifconfig()[0])
+
+
+
+
 # grafische Oberfläche gestalten:
 
-rectangle_title = M5Rect(0, 0, diplay_breite, 20, 0x3366ff, 0x3366ff)
+rectangle_title = M5Rect(0, 0, diplay_breite, 28, 0x3366ff, 0x3366ff)
 label_prog_name = M5TextBox(2, 2, name + ' V:' + version, lcd.FONT_Default, 0x0, rotate=0)
-label_temperatur = M5TextBox(5, 30, temp, lcd.FONT_DejaVu40, 0xFFFFFF, rotate=0)
-label_grad = M5TextBox(90, 26, 'o', lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
-label_celsius = M5TextBox(105, 30, 'C', lcd.FONT_DejaVu40, 0xFFFFFF, rotate=0)
+label_IP = M5TextBox(2, 15, ' IP:' + wlan.ifconfig()[0], lcd.FONT_Default, 0x0, rotate=0)
+label_temperatur = M5TextBox(5, y_offset_temperatur, temp, lcd.FONT_DejaVu40, 0xFFFFFF, rotate=0)
+label_grad = M5TextBox(90, y_offset_temperatur - 4, 'o', lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
+label_celsius = M5TextBox(105, y_offset_temperatur, 'C', lcd.FONT_DejaVu40, 0xFFFFFF, rotate=0)
 
 lcd.clear()
 rectangle_title.show()
 label_prog_name.show()
+label_IP.show()
     
 while True:
     temp = int(KMeter_0.get_kmeter(1))
