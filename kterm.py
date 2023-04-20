97706100c76384251ecb64f694092bcab26497ae
kapest007 <62097668+kapest007@users.noreply.github.com>
2023-04-20 20:18:56 +0200

    Create kterm.py
    Erster commit

---
+++ kterm.py
@@ -0,0 +1,12 @@
+# kterm 
+# ist ein Micropythonscript für ein einfaches Thermometer 
+# mit einem M5Stick C und einer K-Meter Unit.
+
+name = 'kterm.py'
+version = '00.00.000'
+date = '20.04.2023'
+author = 'Peter Stöck'
+
+# TODO:
+
+# Versionen:
